82b8dc18cb548228f222db58929a7a7fb757cb24
Jan Lipponen <jan.lipponen@gmail.com>
2021-12-07 21:16:56 +0200

    Packaging rework

--- setup.py
+++ setup.py
@@ -7,29 +7,6 @@ with io.open('README.md', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    name = "ruuvigate",
     version = ruuvigate.__version__,
-    author = "Jan Lipponen",
-    author_email = "jan.lipponen@gmail.com",
-    description = "An application to push RuuviTag data periodically to cloud",
-    long_description = readme,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/jlipponen/RuuviGate",
-    license='MIT',
-    classifiers = [
-        "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux"
-    ],
-    python_requires=[
-        ">=3.8",
-    ],
-    install_requires=[
-        'ptyprocess;platform_system=="Linux"'
-    ],
-    packages=[
-        'ruuvigate'
-    ],
-    include_package_data=True,
+    long_description = readme
 )

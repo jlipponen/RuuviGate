566df19f4ea8641949562bae1f11f341ac21b185
Jan Lipponen <jan.lipponen@gmail.com>
2021-12-07 21:23:50 +0200

    Skip dynamic version resolving

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 import io
 from setuptools import setup
 
-import ruuvigate
-
 with io.open('README.md', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    version = ruuvigate.__version__,
+    version = "0.1.0",
     long_description = readme
 )

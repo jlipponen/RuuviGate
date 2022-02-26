425f9ee95dad5e0fa9de473e914c5970bde33b23
Jan Lipponen <jan.lipponen@gmail.com>
2022-02-26 16:32:55 +0200

    Added method listener, migrated to async and removed dtor

--- ruuvigate/azure_client.py
+++ ruuvigate/azure_client.py
@@ -3,8 +3,9 @@ import uuid
 import logging
 from enum import Enum
 
-from azure.iot.device import IoTHubDeviceClient
-from azure.iot.device import ProvisioningDeviceClient
+from azure.iot.device.aio import IoTHubDeviceClient
+from azure.iot.device.aio import ProvisioningDeviceClient
+from azure.iot.device import MethodResponse
 from azure.iot.device import Message
 
 
@@ -17,19 +18,14 @@ class AzureClient:
         self.client_ = None
         self.dataBuf_ = {}
 
-    def __del__(self):
-        if self.client_ != None:
-            self.client_.shutdown()
-            self.client_ = None
-
-    def connect(self, device_key: str, device_id: str, iotcentral_hostname: str):
+    async def connect(self, device_key: str, device_id: str, iotcentral_hostname: str):
         try:
             self.client_ = IoTHubDeviceClient.create_from_symmetric_key(
                 symmetric_key=device_key,
                 hostname=iotcentral_hostname,
                 device_id=device_id,
             )
-            self.client_.connect()
+            await self.client_.connect()
         except Exception as ex:
             logging.error(
                 "Unable to connect to Azure! {}: {}".format(
@@ -38,7 +34,29 @@ class AzureClient:
             self.client_ = None
             raise ConnectionError()
 
-    def send_data(self, data={}):
+    async def execute_method_listener(self, method_name, handler):
+        logging.info("Executing a listener for \"" + method_name + "\" method")
+        while True:
+            method_request = await self.client_.receive_method_request(method_name)
+            logging.info("Method request \"" + method_name +
+                         "\" received with payload:\n"+method_request.payload)
+
+            response_payload = await handler(method_request.payload)
+            if response_payload.get("result"):
+                response_status = 200
+            else:
+                response_status = 400
+
+            command_response = MethodResponse.create_from_method_request(
+                method_request, response_status, response_payload
+            )
+
+            try:
+                await self.client_.send_method_response(command_response)
+            except RuntimeError:
+                print("Responding to command request \"{}\" failed".format(method_name))
+
+    async def send_data(self, data={}):
         """
             param data: dictionary of values to send
         """
@@ -49,15 +67,17 @@ class AzureClient:
         msg.content_type = AzureClient.Message.ContentType.value
         msg.message_id = uuid.uuid4()
 
-        self.client_.send_message(msg)
-        logging.info("Sent message " + str(data) +
+        await self.client_.send_message(msg)
+        logging.info("Sent message " + str(msg) +
                      " with id " + str(msg.message_id))
 
-    def buffer_data(self, data):
+        self.dataBuf_.clear()
+
+    async def buffer_data(self, data):
         self.dataBuf_.update(data)
 
     @staticmethod
-    def provision_device(provisioning_host, id_scope, registration_id, symmetric_key, model_id):
+    async def provision_device(provisioning_host, id_scope, registration_id, symmetric_key, model_id):
         provisioning_device_client = ProvisioningDeviceClient.create_from_symmetric_key(
             provisioning_host=provisioning_host,
             registration_id=registration_id,
@@ -66,7 +86,7 @@ class AzureClient:
         )
 
         provisioning_device_client.provisioning_payload = {"modelId": model_id}
-        registration_result = provisioning_device_client.register()
+        registration_result = await provisioning_device_client.register()
 
         if registration_result.status == "assigned":
             logging.info("Device was assigned")

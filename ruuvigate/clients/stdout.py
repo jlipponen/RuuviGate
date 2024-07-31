5763157439580fee5c0627851309e6e06f9ddb6c
Jan Lipponen <jan.lipponen@fi.abb.com>
2024-07-31 16:35:14 +0300

    Added docstrings and type defs for the StdOut class

--- ruuvigate/clients/stdout.py
+++ ruuvigate/clients/stdout.py
@@ -7,12 +7,39 @@ class StdOut:
         self._dataBuf: Dict[str, str] = {}
 
     async def connect(self, _):
+        """
+        Dummy implementation for abstract connect method.
+
+        Args:
+            _: Placeholder argument.
+
+        Returns:
+            None
+        """
         pass
 
-    async def publish_data(self, data={}):
+    async def publish_data(self, data: Dict[str, str] = {}):
+        """
+        Publishes stored and provided data to the standard output.
+
+        Args:
+            data (dict): The data to be published. Defaults to an empty dictionary.
+
+        Returns:
+            None
+        """
         self._dataBuf.update(data)
         print(self._dataBuf)
         self._dataBuf.clear()
 
     async def buffer_data(self, data: Dict[str, str]):
+        """
+        Buffer the provided data into the internal data buffer.
+
+        Args:
+            data (Dict[str, str]): The data to be buffered.
+
+        Returns:
+            None
+        """
         self._dataBuf.update(data)
